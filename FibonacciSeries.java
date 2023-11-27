6b664ae36c250771c9a940d1bec613de666f5dea
jainpresnavm <152098052+jainpresnavm@users.noreply.github.com>
2023-11-26 20:23:19 -0600

    Add files via upload
    Added Fibonacci code

---
+++ FibonacciSeries.java
@@ -0,0 +1,23 @@
+package week1.day2;
+
+public class FibonacciSeries {
+
+	public static void main(String[] args) {
+		int range=8;
+		int first=0;
+		int second=1;
+		int next=0;
+		
+		
+		//program output-0,1,1,2,3,5,8 and 13
+for (int i = 0; i < range; i++) {
+	System.out.println(first +"");
+	next=first+second;
+	first=second;
+	second=next;
+
+	
+}
+	}
+
+}
